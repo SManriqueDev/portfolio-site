a7c79e104546b33fcdc0a56baa155ebe7a9a94a4
Sebastian Manrique <sebastian.manrique@dafiti.com.co>
2022-11-11 13:34:27 -0500

    fix: remove feature graphql query

--- src/components/featured-projects.js
+++ src/components/featured-projects.js
@@ -3,29 +3,29 @@ import * as React from "react"
 import TabButton from "./tab-button"
 import * as jobsStyles from "./jobs.module.css"
 
-export const allStrapiFeaturedProjectsQuery = graphql`
-  {
-    allStrapiProjects(filter: { featured: { eq: true } }) {
-      nodes {
-        title
-        description
-        url
-        github
-        strapiId
-        stack {
-          id
-          name
-        }
-      }
-    }
-  }
-`
+// export const allStrapiFeaturedProjectsQuery = graphql`
+//   {
+//     allStrapiProjects(filter: { featured: { eq: true } }) {
+//       nodes {
+//         title
+//         description
+//         url
+//         github
+//         strapiId
+//         stack {
+//           id
+//           name
+//         }
+//       }
+//     }
+//   }
+// `
 
 const FeaturedProjects = () => {
-  const {
-    allStrapiProjects: { nodes: featuredProjects },
-  } = useStaticQuery(allStrapiFeaturedProjectsQuery)
-  console.log("🚀 ~ file: jobs.js ~ line 25 ~ Jobs ~ nodes", featuredProjects)
+  // const {
+  //   allStrapiProjects: { nodes: featuredProjects },
+  // } = useStaticQuery(allStrapiFeaturedProjectsQuery)
+  // console.log("🚀 ~ file: jobs.js ~ line 25 ~ Jobs ~ nodes", featuredProjects)
 
   return (
     <section
@@ -36,9 +36,9 @@ const FeaturedProjects = () => {
       }}
     >
       <h4 className="section-heading">Algunas cosas que he construido</h4>
-      {featuredProjects.map(project => (
+      {/* {featuredProjects.map(project => (
         <div key={project.strapiId}>{project.title}</div>
-      ))}
+      ))} */}
     </section>
   )
 }
