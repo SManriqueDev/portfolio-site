f93d4d06d777ee74485dbdaa20ee9e0bf116ca36
InmortalRegis <yhoan.manrique@correo.uis.edu.co>
2024-02-23 20:48:49 -0500

    fix: transition hover

--- src/components/project.js
+++ src/components/project.js
@@ -7,15 +7,18 @@ const Project = ({ project }) => {
   return (
     <div className={projectStyles.project} key={project.strapiId}>
       <div className={projectStyles.projectImage}>
-        <Image
-          fluid={project.picture.localFile.childImageSharp.fluid}
-          className={projectStyles.picture}
-          alt="Project image"
-          objectFit="fill"
-          style={{
-            height: "350px",
-          }}
-        />
+        <a href={project.url} target="_new">
+          <Image
+            fluid={project.picture.localFile.childImageSharp.fluid}
+            className={projectStyles.picture}
+            alt="Project image"
+            objectFit="cover"
+            style={{
+              height: "100%",
+              maxWidth: "100%",
+            }}
+          />
+        </a>
       </div>
       <div className={projectStyles.projectContent}>
         <p className={projectStyles.projectOverline}>Proyecto destacado</p>
